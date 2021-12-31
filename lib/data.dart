def04c0d72e0559b4d6ab6352dd450edbd3d5eba
Manoj Padiya <manoj.p@simformsolutions.com>
2021-12-31 13:52:04 +0530

    Initial commit

---
+++ lib/data.dart
@@ -0,0 +1,1 @@
+var myDataList = ['column', 'row', 'textField'];
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:basic_widget/data.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyHomePage(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Widget Example"),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/${myDataList[index]}');
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 15.0),
+                child: Text(
+                  myDataList[index],
+                  style: const TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+        itemCount: myDataList.length,
+      ),
+    );
+  }
+}
